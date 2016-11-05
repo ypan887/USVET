5108a5913eb4e2357d71a9e4bdd173fd8d957c3b
ypan887 <ypan887@gmail.com>
2016-11-04 22:26:23 -0700

    add learn about, volunteer, and donation page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+  layout "index_page", only: [:index]
+
   def index
+  end
+
+  def learn_about
+  end
+
+  def volunteer
+  end
+
+  def donation
   end
 end
