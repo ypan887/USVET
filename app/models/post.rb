acb189d7af52c404591cb53f899ed44751a8b0fd
ypan887 <ypan887@gmail.com>
2016-11-06 16:56:05 -0800

    implemented recent posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :admin_user
 
+  scope :recent_posts, -> { order(created_at: :desc).limit(5) }
   ransacker :user_name_eq,
             :formatter => ->(n) {
               AdminUser.where(user_name: name).id
