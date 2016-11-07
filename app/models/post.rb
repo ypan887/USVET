d77266583520994875259afd7c2869290cb87997
ypan887 <ypan887@gmail.com>
2016-11-06 17:51:54 -0800

    implement post archives that display posts based on their post date

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :admin_user
 
   scope :recent_posts, -> { order(created_at: :desc).limit(5) }
+  scope :archives, -> { 
+    all.group_by{|t| t.created_at.beginning_of_month.strftime("%Y-%B")}
+  }
+
   ransacker :user_name_eq,
             :formatter => ->(n) {
               AdminUser.where(user_name: name).id
