70ea47949f6acf7e20c4f83e9f62b1cfeaf66016
ypan887 <ypan887@gmail.com>
2016-11-08 15:02:08 -0800

    add fix to ckeditor on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w(ckeditor/*)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
