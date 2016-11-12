82dc3af6b18c1b111e50e546c44601024a9ba75b
ypan887 <ypan887@gmail.com>
2016-11-11 16:05:28 -0800

    add validations to answer model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+  validates_presence_of :content
+  validates_presence_of :question_id
+  validates_presence_of :end_question
+  validate :with_next_question, :end_of_question, :question_loop
+
   belongs_to :admin_user
 
   scope :recent_posts, -> { order(created_at: :desc).limit(5) }
@@ -12,4 +17,25 @@ class Post < ApplicationRecord
             } do |parent|
               parent.table[:id]
             end
+
+private
+  def with_next_question 
+    if !end_question && next_question_id
+      errors.add(:next_question_id, "please specify next question") unless next_question_id
+      errors.add(:value, "this answer do not need value") if value
+    end
+  end
+
+  def end_of_question
+    if end_question
+      errors.add(:value, "please specify the result content") if value.nil?
+      errors.add(:next_question_id, "this answer is the end result") if next_question_id
+    end
+  end
+
+  def question_loop
+    if next_question_id && next_question_id == question_id
+      errors.add(:next_question_id, "please choose another question to avoid infinite loop")
+    end 
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
+  validates_presence_of :content
   validates_uniqueness_of :base, if: :base
   has_many :answers
 end
