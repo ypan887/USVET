576f382833b730d87714e752a7b539cb619e1743
ypan887 <ypan887@gmail.com>
2016-11-16 12:50:57 -0800

    since treant js does not support multiple parent, so the decision tree view will not display answer node for now

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -10,11 +10,10 @@ class Answer < ApplicationRecord
   def self.to_tree
     answers = all.includes(:question, :next_question)
     tree = answers.inject({}) do |result, a|
-      result["answers_#{a.id}"] = a.answer_node
       result["questions_#{a.question_id}"] = a.question_node unless result.has_key?("questions_#{a.question_id}")      
       if a.next_question_id
         next_question = result["questions_#{a.next_question_id}"]
-        next_question["parent"] = "answers_#{a.id}" if next_question
+        next_question["parent"] = "questions_#{a.question_id}" if next_question
         result["questions_#{a.next_question_id}"] ||= a.next_question_node
       end
       result
@@ -22,16 +21,12 @@ class Answer < ApplicationRecord
     tree.values
   end
 
-  def answer_node
-    { "name" => "answers_#{id}", "parent"=>"questions_#{question_id}", "text"=> content }
-  end
-
   def question_node
     { "name" => "questions_#{question_id}", "text"=> question.content }
   end
 
   def next_question_node
-    { "name" => "questions_#{next_question_id}", "parent"=>"answers_#{id}", "text"=> next_question.content }
+    { "name" => "questions_#{next_question_id}", "parent" => "questions_#{question_id}", "text"=> next_question.content }
   end
 
 private
