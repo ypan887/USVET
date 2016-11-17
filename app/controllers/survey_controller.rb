2c9cc9db1f0529d519254878dbe983dfc85e17b7
ypan887 <ypan887@gmail.com>
2016-11-16 16:07:18 -0800

    add go back buttong to QA system

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,6 +1,7 @@
 class SurveyController < ApplicationController
   def index
     initial_question
+    @stack_size = @@question_stack.size
   end
 
   def show
@@ -8,7 +9,8 @@ class SurveyController < ApplicationController
   end
 
   def create
-    @new_question = Question.where(id: params[:next_question]).includes(:answers).first
+    @new_question = update_question
+    @stack_size = @@question_stack.size
     respond_to do |format|
       format.js
     end
@@ -18,6 +20,26 @@ private
   def initial_question
     @question = Question.base_question.first
     @question ||= Question.includes(:answers).first
+    @@question_stack = [@question]
+  end
+
+  def update_stack
+    question = Question.where(id: params[:next_question]).includes(:answers).first
+    @@question_stack << question
+    question
+  end
+
+  def pop_stack
+    @@question_stack.pop
+    @@question_stack.last
+  end
+
+  def update_question
+    if params[:next_question]
+      update_stack
+    else
+      pop_stack
+    end
   end
 
   def current_answer(id)
